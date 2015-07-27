29d0398bccb81960327e2dfff7829b04f7828ca9
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2015-07-27 01:01:44 -0600

    add images, content

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+
+		<!-- Optional Bootstrap theme -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" rel="stylesheet"/>
+
+		<!--custom css-->
+		<link type="text/css" href="css/style.css" rel="stylesheet"/>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.1/additional-methods.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<title>Bootstrap Sample Page</title>
+	</head>
+	<body class="sfooter">
+
+		<!--this wrapper div is for the sticky footer-->
+		<div class="sfooter-content">
+
+			<!--begin page header-->
+			<header>
+				<div class="container">
+					<h1>Sample Bootstrap Page</h1>
+					<ul class="nav nav-pills pull-right">
+						<li role="presentation"><a href="#">Home</a></li>
+						<li role="presentation"><a href="#">Profile</a></li>
+						<li role="presentation"><a href="#">Messages</a></li>
+					</ul>
+				</div>
+			</header>
+
+			<!--begin main body content-->
+			<main class="container">
+
+				<!--first content row, full-width-->
+				<div class="row">
+					<div class="col-xs-12">
+						<img src="img/i-can-has-cheezburger.jpg" alt="cheezburger" class="img-responsive pull-left img-thumbnail">
+						<p>The Federation's gone; the Borg is everywhere! We could cause a diplomatic crisis. Take the ship into the Neutral Zone Besides, you look good in a dress. Mr. Crusher, ready a collision course with the Borg ship. Fate. It protects fools, little children, and ships named "Enterprise." Sure. You'd be surprised how far a hug goes with Geordi, or Worf. Sorry, Data. Now, how the hell do we defeat an enemy that knows us better than we know ourselves? Is it my imagination, or have tempers become a little frayed on the ship lately? Shields up! Rrrrred alert! Why don't we just give everybody a promotion and call it a night - 'Commander'?</p>
+					</div>
+				</div>
+
+
+				<!--second content row, split-->
+				<div class="row">
+					<div class="col-md-6">
+						<p>Smooth as an android's bottom, eh, Data? A surprise party? Mr. Worf, I hate surprise parties. I would *never* do that to you. Now, how the hell do we defeat an enemy that knows us better than we know ourselves? They were just sucked into space. My oath is between Captain Kargan and myself. Your only concern is with how you obey my orders. Or do you prefer the rank of prisoner to that of lieutenant? Besides, you look good in a dress. What's a knock-out like you doing in a computer-generated gin joint like this? Worf, It's better than music. It's jazz. We finished our first sensor sweep of the neutral zone. Your shields were failing, sir. Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? I can't. As much as I care about you, my first duty is to the ship</p>
+					</div>
+					<div class="col-md-6">
+						<p>Smooth as an android's bottom, eh, Data? A surprise party? Mr. Worf, I hate surprise parties. I would *never* do that to you. Now, how the hell do we defeat an enemy that knows us better than we know ourselves? They were just sucked into space. My oath is between Captain Kargan and myself. Your only concern is with how you obey my orders. Or do you prefer the rank of prisoner to that of lieutenant? Besides, you look good in a dress. What's a knock-out like you doing in a computer-generated gin joint like this? Worf, It's better than music. It's jazz. We finished our first sensor sweep of the neutral zone. Your shields were failing, sir. Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? I can't. As much as I care about you, my first duty is to the ship</p>
+					</div>
+				</div>
+
+			</main>
+		</div>
+
+		<!--begin footer-->
+		<footer>
+			<div class="container">
+				<p>&copy;&nbsp;2015 Last Minute LLC.</p>
+			</div>
+		</footer>
+	</body>
+</html>
