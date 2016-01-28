9731bd8c7d7fd38184601bbf868c8c64c8125c89
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2016-01-28 16:01:55 -0700

    update html, css with new class wireframe example

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
 		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-		<!-- Bootstrap latest complied and minified CSS -->
-		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-		<!-- Optional Bootstrap theme -->
-		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
-		<!-- Custom CSS -->
+		<!-- My Custom CSS rlewis37@cnm.edu -->
 		<link rel="stylesheet" href="css/style.css" type="text/css"/>
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -23,108 +23,112 @@
 		<![endif]-->
 
 		<!-- jQuery (required for Bootstap's JS plugins) -->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 
-		<!-- Bootstap latest complied and minified JavaScript -->
-		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha256-Sk3nkD6mLTMOF0EOpNtsIry+s1CsaqQC1rVLTAy+0yc= sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
 		<title>Basic Bootstrap Wireframe Exercise</title>
 	</head>
 	<body class="sfooter">
 		<div class="sfooter-content">
+
+			<!--begin header and navbar-->
 			<header>
 				<div class="container">
-					<div class="row">
-						<div class="col-md-12">
-							<!--begin navbar-->
-							<nav class="navbar navbar-inverse">
-
-								<!--logo and mobile toggle button get grouped together-->
-								<div class="navbar-header">
-									<button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#my-navbar" aria-expanded="false">
-										<span class="sr-only">Main Menu</span>
-										<span class="glyphicon glyphicon-plus"></span>
-									</button>
-									<a href="#" class="navbar-brand">
-										<span class="glyphicon glyphicon-equalizer"></span>
-										Bootstrap Wireframe
-									</a>
-								</div>
-
-								<!--nav links are grouped together here -->
-								<div class="collapse navbar-collapse navbar-right" id="my-navbar">
-									<ul class="nav navbar-nav">
-										<li><a href="#">Link</a></li>
-										<li><a href="#">Link</a></li>
-										<li><a href="#">Link</a></li>
-									</ul>
-								</div>
-							</nav>
+					<!--Begin the Bootstrap Navbar - see documentation: http://getbootstrap.com/components/#navbar -->
+					<nav class="navbar navbar-inverse">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">Bootstrap Wireframe Challenge!</a>
 						</div>
-					</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="#">Catnip</a></li>
+								<li><a href="#">Toys</a></li>
+								<li><a href="#">Kitty Treats</a></li>
+								<li><a href="https://senator-arlo.bowtied.io/" target="_blank">Feel the Fuzzy</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</nav>
 				</div>
 			</header>
+
+			<!--begin main page content-->
 			<main>
 				<div class="container">
+
+					<!--begin welcome row-->
 					<div class="row">
-						<div class="col-xs-12">
+						<div class="col-md-12">
 							<div class="jumbotron">
-								<h1>Basic Bootstrap Wireframe</h1>
-								<p>This example uses the Bootstrap navbar, Jumbotron, and the Flexbox Sticky Footer.</p>
-								<div>View Source for details. Shrink your browser window down to see this layout's responsive behavior in action.</div>
+								<h1>Meow! =^. .^=</h1>
+								<p>Welcome to Senator Arlo's Bootstrap page!</p>
 							</div>
 						</div>
 					</div>
-					<div class="row">
-						<div class="col-md-3">
-							<div class="text-box bg-pink">
-								<h3>Try-hard Kitsch</h3>
-								<p>Four loko ethical williamsburg austin lo-fi try-hard kitsch, 90's cliche lomo fanny pack poutine. Venmo blog viral wayfarers before they sold out fingerstache.</p>
-							</div>
-						</div>
-						<div class="col-md-3">
-							<div class="text-box bg-pink">
-								<h3>Art Party Swag</h3>
-								<p>Meh waistcoat kitsch shabby chic. Roof party tumblr thundercats kombucha intelligentsia tote bag, heirloom ugh readymade art party swag.</p>
-							</div>
-						</div>
-						<div class="col-md-3">
-							<div class="text-box bg-pink">
-								<h3>Fingerstache</h3>
-								<p>Kitsch squid hoodie vice kale chips messenger bag. Microdosing farm-to-table synth shoreditch. Portland roof party health goth, beard artisan truffaut.</p>
-							</div>
-						</div>
-						<div class="col-md-3">
-							<div class="text-box bg-pink">
-								<h3>Cliche Waistcoat</h3>
-								<p>Kitsch microdosing XOXO umami cold-pressed, jean shorts pabst. Cliche waistcoat crucifix selvage squid hashtag wayfarers sustainable street art locavore brunch.</p>
-							</div>
-						</div>
-					</div>
+
+					<!--begin 2 column row-->
 					<div class="row">
 						<div class="col-md-6">
-							<div class="text-box bg-gray">
-								<h3>About This Layout</h3>
-								<p>Bootstrap default styles have been used primarily throughout. View CSS source to see the custom styles.</p>
+							<div class="content-box">
+								<h3>Navbar</h3>
+								<p>This example uses Bootstrap's built-in Navbar. RTFM here: <a
+										href="http://getbootstrap.com/components/#navbar" target="_blank">http://getbootstrap.com/components/#navbar</a></p>
 							</div>
 						</div>
 						<div class="col-md-6">
-							<div class="text-box bg-gray">
-								<h3>About This Code</h3>
-								<p>View the source for the CSS to see the Flexbox Sticky Footer. This Flexbox hack will keep a footer at the bottom of a page even if the content above it is short.</p>
+							<div class="content-box">
+								<h3>Flexbox Sticky Footer</h3>
+								<p>Keep the footer at the bottom of the page, always. Even if the content-area is "short". This is the finest sticky footer I've ever used. View the source code.</p>
 							</div>
 						</div>
 					</div>
+
+					<!--begin 4 column row-->
+					<div class="row">
+						<div class="col-md-3">
+							<div class="content-box">
+								<h3>Jumbotron</h3>
+								<p>The Bootstrap "jumbotron" made the "Meow!" big. This is also sometimes referred to as a "Hero Message", lol. RTFM <a href="http://getbootstrap.com/components/#jumbotron" target="_blank">here</a>.</p>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="content-box">
+								<h3>Box Styling</h3>
+								<p>See these fancy round-corner boxes? I did that with some custom CSS. Open devtools and look for the &lt;div class="content-box"&gt;. Yay!</p>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="content-box">
+								<h3>Mobile First</h3>
+								<p>Check this page out on your mobile device! Bootstrap powers the responsive behavior by default when build websites the Bootstrap way. :D</p>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="content-box">
+								<h3>View Source</h3>
+								<p>This is a lotta Bootstrap, I know. View the source code, it's well commented. Take a look in devtools. Try stuff. Break things. Have fun.</p>
+							</div>
+						</div>
+					</div>
+
 				</div>
 			</main>
-		</div>
+		</div><!--.sfooter-content-->
+
+		<!--begin footer content-->
 		<footer>
 			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<div>This is some fancy footer text :D</div>
-					</div>
-				</div>
+				&copy; 2016 Back Paws for Bernie Campaign
 			</div>
 		</footer>
 	</body>
